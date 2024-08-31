7a93e85e243a020c00d02629e7b3ff67326ede4d
Vikash khowal <149614341+vk-sigma17@users.noreply.github.com>
2024-08-31 16:42:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Navbar from './component/Navbar';
 import Category from './component/category';
 import Restaurant from './component/Restaurant';
@@ -7,13 +7,13 @@ import RestautantDelivery from './component/RestautantDelivery';
 import Boxes from './component/Boxes';
 import Download from './component/Download';
 import Footer from './component/Footer';
-import Cart from './pages/Cart'
+
 
 
 export default function App(){
   return(
     <div>
-      <BrowserRouter>
+
         <Navbar />
         <Category />
         <Restaurant />
@@ -21,13 +21,9 @@ export default function App(){
         <Boxes />
         <Download />
         <Footer />
-        <Routes>
-          {/* <Route path='/' element={<Home />}></Route> */}
-          <Route path='/Cart' element={<Cart />}></Route>
-         
-        </Routes>
+        
 
-      </BrowserRouter>
+
     </div>
   )
 }
