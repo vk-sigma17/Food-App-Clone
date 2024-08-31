415bd41d71701f27e1d92c9d8742a5b5b623de44
Vikash khowal <149614341+vk-sigma17@users.noreply.github.com>
2024-08-31 16:40:51 +0530

    Delete src/pages directory

--- src/pages/Cart.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Cart(){
-    return (
-        <div>
-            <h2>CART Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, inventore.</h2>
-        </div>
-    )
-}
